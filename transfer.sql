3f8e9e82ae1382f23c3f9edd7ad61ad64765cfb0
Ridhohertaputra <131152285+Ridhohertaputra@users.noreply.github.com>
2023-09-15 19:09:42 +0700

    Update transfer.sql

--- transfer.sql
+++ transfer.sql
@@ -1,9 +1,9 @@
 CREATE TABLE transfer (
-    No_Rek INT(50) PRIMARY KEY,
-    No_Resi INT(35),
+    No_Resi INT(35) PRIMARY KEY,
+    No_Rek INT(50),
     No_Rek_Tujuan INT(50),
     Asal_Bank VARCHAR(15),
     Bank_Tujuan VARCHAR(15),
     Nominal_Transaksi INT(70),
-    FOREIGN KEY (No_Resi) REFERENCES nasabah(No_Rek)
+    FOREIGN KEY (No_Rek) REFERENCES nasabah(No_Rek)
 );
